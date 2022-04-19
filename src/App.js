026917f23cc9f21f31c508f01dd1bf270bed06a4
LAPTOP-6OMKD8F4\Studnets <tabuashvili.anna@kiu.edu.ge>
2022-04-19 23:42:47 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "footer">
+      <p>Kutaisi International University is a public <br /> research university in kutaisi
+      , Georgia. It's <br /> construction was announced on 12 <br /> september 2016. 
+      <br /> @2002 All Copyright Reserved</p>
+      <ul> <li><a href="#">About us</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#"> Random page </a></li>
+      </ul>
+      <img src="https://www.kiu.edu.ge/images/logo_geo2.png" alt="KIU" />
+      
     </div>
   );
+  
+   
+  
 }
 
 export default App;
